3a98e18e7e85de3fa0e45a0633cfcd0281fed93e
Andrew <chebanov.andrew@gmail.com>
2024-09-22 22:48:33 +0200

    SA optimization

--- main.cpp
+++ main.cpp
@@ -1,141 +1,165 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 #include <fstream>
 #include <sstream>  
 #include <map>
-#include "A*/A_star.hpp"
+#include "Area/area.hpp"
 #include "reader_csv.hpp"
-#include "FP-grown/fp-grown.hpp"
+
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
 
 template <size_t square_side>
-void add_items(std::vector<Square<square_side>*> loc, std::vector<std::string>::iterator& it, std::map<std::string, Square<square_side>*>& data, size_t n) {
+void add_items(std::vector<Square<square_side>*> loc, std::vector<std::string>::iterator& it, std::unordered_map<std::string, Square<square_side>*>& data, size_t n) {
     
     for(auto* location: loc) {
         for(size_t count = 0; count < n; ++count, ++it) {
-            data.insert(std::pair<std::string, Square<2>*>(std::move(*it), location));
+            data.insert(std::pair<std::string, Square<2>*>(*it, location));
         }
     }
 }
 
+template<size_t side>
+double total_cost(std::vector<std::string>& transactions, Square<side>* start, std::unordered_map<std::string, Square<2>*>& locations) {
+    double curr_cost = 0;
+
+    for(const auto& item: transactions) {
+        auto item_it = locations.find(item);
+        if(item_it != locations.end()) {
+                curr_cost += euclidian_distance(start, item_it->second);
+                start = item_it->second;
+        }
+        else {
+            throw std::logic_error("Not found");
+        }
+    }
+
+    return curr_cost;
+}
+
+template<size_t side>
+void start_state(Area<side>& warehouse, std::vector<std::string>& items, std::unordered_map<std::string, Square<side>*>& locations) {
+    auto it = items.begin();
+    
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(0, 1), std::pair<size_t, size_t>(0, 6)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 3), std::pair<size_t, size_t>(8, 4)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 1), std::pair<size_t, size_t>(8, 1)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 6), std::pair<size_t, size_t>(8, 6)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(10, 1), std::pair<size_t, size_t>(10, 6)), it, locations, 1);
+}
+
+template<size_t side>
+void generate_new_state(Area<side>& warehouse, std::vector<std::string>& items, std::unordered_map<std::string, Square<side>*>& locations) {
+    std::uniform_int_distribution<size_t> dist(0, items.size()-1);
+
+    size_t i = dist(gen);
+    size_t j = dist(gen);
 
 
-auto fp_location(std::vector<std::string>& records, std::vector<std::string>& items_by_popularity) {
-    FP_Tree tree;
-    std::vector<std::string> result;
-
-    auto transactions = get_ordered_transactions(records, 60);
-        
-    for(auto transaction: transactions) {
-        tree.insert(transaction);
+    if(i > j) {
+        std::reverse(items.begin() + j, items.end() - i);
+    }
+    else {
+        std::reverse(items.begin() + i, items.end() - j);
     }
     
-    auto association_map = getAssociation_map(items_by_popularity, tree);
+    auto it = items.begin();
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(0, 1), std::pair<size_t, size_t>(0, 6)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 3), std::pair<size_t, size_t>(8, 4)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 1), std::pair<size_t, size_t>(8, 1)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(2, 6), std::pair<size_t, size_t>(8, 6)), it, locations, 1);
+    add_items<side>(warehouse.getSquares(std::pair<size_t, size_t>(10, 1), std::pair<size_t, size_t>(10, 6)), it, locations, 1);
 
-    std::unordered_set<std::string> closed;
+}
 
-    for(auto& product: items_by_popularity) {
-        if(closed.find(product) != closed.end()) {
-            continue;
-        }
-        
-        closed.emplace(product);
-        result.push_back(product);
-        if(association_map.find(product) != association_map.end()) { 
-
-            std::string sel_item = "none";
-            for(auto& [item, counter1]: association_map.find(product)->second) {
-                if(closed.find(item) == closed.end()) {
-                    sel_item = item;
-                    closed.emplace(item);
-                    result.push_back(item);
-                    break;
-                }
-            }
-
-            if(sel_item == "none") {
-                continue;
-            }
-
-            for(auto& [item2, counter2]: association_map.find(sel_item)->second) {
-                        
-                if(closed.find(item2) == closed.end()) {
-                    closed.emplace(item2);
-                    result.push_back(item2);
-                    break;
-                }
-             
-            }
-                    
-        }        
+bool make_transition(long double P) {
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    if(P >= dis(gen)) {
+        return true;
     }
-    return result;
+
+    return false;
 }
 
 
 
+template<size_t side>
+void sa_optimization(double start_temp, double end_temp, double cooling_rate, size_t steps, Area<side>& warehouse, Square<side>* start) {
+    std::unordered_map<std::string, Square<side>*> locations;
+    std::vector<std::string> records;
+    
+    std::ifstream file_csv("Data/reduced_csv/train_r.csv");
+    get_records(file_csv, records);
+    file_csv.close();
 
+    auto transactions = get_transactions(records);
+    std::vector<std::string> items;    
+    
+    std::ifstream file2("Data/products_names/reduced_pop.txt");
+    get_product_names(file2, items);
+    file2.close();
 
+    start_state<side>(warehouse, items, locations);
+
+    double currEnergy = total_cost<side>(transactions, start, locations);
+    double T = start_temp;
+    
+    size_t i = 0;
+    while(i < steps && currEnergy != 0) {
+
+        double newEnergy = 0;
+        
+        Area<side> new_warehouse(warehouse.getWidth(), warehouse.getHeight());
+        std::unordered_map<std::string, Square<side>*> new_locations;
+        std::vector<std::string> new_items = items; 
+
+        generate_new_state<side>(new_warehouse, new_items, new_locations);
+     
+        
+        newEnergy = total_cost<side>(transactions, start, new_locations);
+        
+
+        if(newEnergy < currEnergy) {
+            currEnergy = newEnergy;
+
+            warehouse = new_warehouse;
+            locations = std::move(new_locations);
+            items = std::move(new_items);
+        }
+        else {
+            if(make_transition(exp(-((newEnergy-currEnergy)/T)))) {
+                currEnergy = newEnergy;
+
+                warehouse = new_warehouse;
+                locations = std::move(new_locations);
+                items = std::move(new_items);
+            }
+   
+        }
+
+        T = start_temp / (1 + cooling_rate * i); 
+
+        if(T <= end_temp) {
+            return ;
+        }
+
+        ++i;
+        
+        std::cout << i << "   " << T << std::endl; 
+    }
+
+    std::cout << std::fixed << std::setprecision(5) << currEnergy << std::endl;
+}
 
 
 int main() {
     
     Area<2> warehouse(11, 7);
-    
-    std::map<std::string, Square<2>*> products_loc;
-    std::vector<std::string> records;
-
-    std::ifstream file_csv("Data/run_data.csv");
-    get_records(file_csv, records);
-    file_csv.close();
- 
-
-    std::vector<std::string> items_by_popularity;
-    
-    std::ifstream file2("Data/most_popular.txt");
-    get_product_names(file2, items_by_popularity);
-    file2.close();
-
-    // use association_map
-    auto items_fp_loc = fp_location(records, items_by_popularity);
-    auto it = items_fp_loc.begin();
-
-    // simply put in shelfs most popular item first
-    //auto it = items_by_popularity.begin();
-    
-    
-    // poor location
-    // add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(0, 1), std::pair<size_t, size_t>(0, 6)), it, products_loc, 3);
-    // add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 1), std::pair<size_t, size_t>(8, 1)), it, products_loc, 3);
-    // add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(10, 1), std::pair<size_t, size_t>(10, 6)), it, products_loc, 3);
-    // add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 6), std::pair<size_t, size_t>(8, 6)), it, products_loc, 3);
-    // add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 3), std::pair<size_t, size_t>(8, 4)), it, products_loc, 3);
-
-    //naive location
-    add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(0, 1), std::pair<size_t, size_t>(0, 6)), it, products_loc, 3);
-    add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 3), std::pair<size_t, size_t>(8, 4)), it, products_loc, 3);
-    add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 1), std::pair<size_t, size_t>(8, 1)), it, products_loc, 3);
-    add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(2, 6), std::pair<size_t, size_t>(8, 6)), it, products_loc, 3);
-    add_items<2>(warehouse.getSquares(std::pair<size_t, size_t>(10, 1), std::pair<size_t, size_t>(10, 6)), it, products_loc, 3);
-
-
     Square<2>* start = warehouse.getSquare(0, 0);
-    size_t user_path = 0;
-    auto tr_item_map = get_transactions(records);
 
-    for(const auto& items: tr_item_map) {
-        for(const auto& item: items) {
-            auto item_it = products_loc.find(item);
-            if(item_it != products_loc.end()) {
-    
-                user_path += calculate_total_cost(a_search<2>(warehouse, start, item_it->second));
-                start = item_it->second;
-
-                warehouse.reset_squares();
-            }
-        }
-    }
-
-    std::cout << user_path;
+    sa_optimization<2>(100000, 0.01, 0.99, 100000, warehouse, start);
 }
-
